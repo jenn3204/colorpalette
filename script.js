c2241d57ff0bc9c3de5f77e77715f9cf7aca1aad
jenniferjaque <jenn3204@stud.kea.dk>
2020-02-19 23:30:30 +0100

    only base working

--- script.js
+++ script.js
@@ -10,9 +10,35 @@ function changeColor() {
   let selectedColor = event.target.value;
 
   document.querySelector("#colorbox").style.backgroundColor = selectedColor;
+  document.querySelector("#colorbox1").style.backgroundColor = selectedColor;
+  document.querySelector("#colorbox2").style.backgroundColor = selectedColor;
+  document.querySelector("#colorbox3").style.backgroundColor = selectedColor;
+  document.querySelector("#colorbox4").style.backgroundColor = selectedColor;
+
   document.querySelector("#hex").value = selectedColor.toUpperCase();
+  document.querySelector("#hex1").value = selectedColor.toUpperCase();
+  document.querySelector("#hex2").value = selectedColor.toUpperCase();
+  document.querySelector("#hex3").value = selectedColor.toUpperCase();
+  document.querySelector("#hex4").value = selectedColor.toUpperCase();
+
   document.querySelector("#rgb").value = hex2rgb();
+  document.querySelector("#rgb1").value = hex2rgb();
+  document.querySelector("#rgb2").value = hex2rgb();
+  document.querySelector("#rgb3").value = hex2rgb();
+  document.querySelector("#rgb4").value = hex2rgb();
+
   document.querySelector("#hsl").value = rgb2hsl();
+  document.querySelector("#hsl1").value = rgb2hsl();
+  document.querySelector("#hsl2").value = rgb2hsl();
+  document.querySelector("#hsl3").value = rgb2hsl();
+  document.querySelector("#hsl4").value = rgb2hsl();
+
+  // document.querySelector("#analogues").addEventListener("change", anaPicked);
+  if (document.querySelector("#analogues").checked == true) {
+    anaPicked();
+  }
+
+  //document.querySelector("#analogues").addEventListener("click", anaPicked);
 
   console.log(hex);
 }
@@ -82,5 +108,23 @@ function rgb2hsl() {
   s = Math.floor(s);
   l = Math.floor(l);
   console.log(h, s, l);
-  return `${h}%, ${s}%, ${l}%`;
+  return `hsl(${h}, ${s}%, ${l}%)`;
+}
+
+function hslNumbers() {
+  let hsl = rgb2hsl();
+}
+
+function anaPicked() {
+  console.log("hej med dig");
+  let selectedColor = event.target.value;
+  console.log(selectedColor);
+  console.log(rgb2hsl());
+
+  // selectedHSL = rgb2hsl();
+  // console.log("hej " + selectedHSL);
+  // h = selectedHSL[5];
+
+  document.querySelector("#colorbox1").style.backgroundColor = selectedColor;
+  document.querySelector("#colorbox2").style.backgroundColor = rgb2hsl();
 }
