5403cb359c8c90f5504a919e71d5525b99f697d2
jenniferjaque <jenn3204@stud.kea.dk>
2020-02-19 11:48:45 +0100

    completed

--- script.js
+++ script.js
@@ -3,26 +3,23 @@
 window.addEventListener("DOMContentLoaded", start);
 
 function start() {
-  document.querySelector("#colorbox").style.backgroundColor = "black";
-  document.querySelector("#hex").textContent = "HEX: #CCCCCC";
-
   document.querySelector("#color_wheel").addEventListener("change", changeColor);
 }
 
 function changeColor() {
-  //const hex = event.target.value;
-  document.querySelector("#colorbox").style.backgroundColor = event.target.value;
-  document.querySelector("#hex").textContent = "HEX: " + event.target.value.toUpperCase();
+  let selectedColor = event.target.value;
+
+  document.querySelector("#colorbox").style.backgroundColor = selectedColor;
+  document.querySelector("#hex").value = selectedColor.toUpperCase();
+  document.querySelector("#rgb").value = hex2rgb();
+  document.querySelector("#hsl").value = rgb2hsl();
 
   console.log(hex);
-
-  const selectedColor = event.target.value;
-  hex2rgb(selectedColor);
-
-  document.querySelector("#rgb").textContent = hex2rgb();
 }
 
-function hex2rgb(selectedColor) {
+function hex2rgb() {
+  let selectedColor = event.target.value;
+
   const hex1 = selectedColor.substring(1, 3);
   const hex2 = selectedColor.substring(3, 5);
   const hex3 = selectedColor.substring(5, 7);
@@ -33,8 +30,57 @@ function hex2rgb(selectedColor) {
   const b = parseInt(hex3, 16);
   console.log(r, g, b);
 
-  const rgb = { r, g, b };
-  return rgb;
+  return `${r} ${g} ${b}`;
 }
 
-function rgb2hsl() {}
+function rgb2hsl() {
+  let rgbColors = hex2rgb();
+  const firstSpace = rgbColors.indexOf(" ");
+  const lastSpace = rgbColors.lastIndexOf(" ");
+
+  let r = rgbColors.substring(0, firstSpace);
+  let g = rgbColors.substring(firstSpace, lastSpace);
+  let b = rgbColors.substring(lastSpace, rgbColors.length);
+
+  console.log("hej" + r, g, b);
+
+  r /= 255;
+  g /= 255;
+  b /= 255;
+
+  let h, s, l;
+
+  const min = Math.min(r, g, b);
+  const max = Math.max(r, g, b);
+
+  if (max === min) {
+    h = 0;
+  } else if (max === r) {
+    h = 60 * (0 + (g - b) / (max - min));
+  } else if (max === g) {
+    h = 60 * (2 + (b - r) / (max - min));
+  } else if (max === b) {
+    h = 60 * (4 + (r - g) / (max - min));
+  }
+
+  if (h < 0) {
+    h = h + 360;
+  }
+
+  l = (min + max) / 2;
+
+  if (max === 0 || min === 1) {
+    s = 0;
+  } else {
+    s = (max - l) / Math.min(l, 1 - l);
+  }
+
+  s *= 100;
+  l *= 100;
+
+  h = Math.floor(h);
+  s = Math.floor(s);
+  l = Math.floor(l);
+  console.log(h, s, l);
+  return `${h}%, ${s}%, ${l}%`;
+}
